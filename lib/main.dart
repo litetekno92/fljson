7fae14c4d7deafc07190909143b0a808acca698a
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-12 09:36:58 +0200

    Comments removed from main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:fljson/photo.dart';
-import 'package:http/http.dart' as http;
 import 'package:fljson/API.dart';
 
 class MainFetchData extends StatefulWidget {
@@ -12,8 +11,8 @@ class MainFetchData extends StatefulWidget {
 
 class _MainFetchDataState extends State<MainFetchData> {
   // List<Photo> list = List();
-    var list = new List<Photo>();
-    var isLoading = false;
+  var list = new List<Photo>();
+  var isLoading = false;
 
   _fetchData() {
     setState(() {
@@ -28,27 +27,7 @@ class _MainFetchDataState extends State<MainFetchData> {
     });
   }
 
-  // var isLoading = false;
-
-  // _fetchData() async {
-  //   setState(() {
-  //     isLoading = true;
-  //   });
-  //   final response =
-  //       await http.get("https://jsonplaceholder.typicode.com/photos");
-  //   if (response.statusCode == 200) {
-  //     list = (json.decode(response.body) as List)
-  //         .map((data) => new Photo.fromJson(data))
-  //         .toList();
-  //     setState(() {
-  //       isLoading = false;
-  //     });
-  //   } else {
-  //     throw Exception('Failed to load photos');
-  //   }
-  // }
-
-  initState() {
+    initState() {
     super.initState();
     _fetchData();
   }
@@ -57,39 +36,18 @@ class _MainFetchDataState extends State<MainFetchData> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Fetch Data JSON"),
         ),
-        // bottomNavigationBar: Padding(
-        //   padding: const EdgeInsets.all(8.0),
-        //   child: RaisedButton(
-        //     child: new Text("Fetch Data"),
-        //     onPressed: _fetchData,
-        //   ),
-        // ),
-        body: 
-        // isLoading   ? Center(
-        //         child: CircularProgressIndicator(),
-        //       )
-        //     :
-             ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    contentPadding: EdgeInsets.all(10.0),
-                    title: new Text(list[index].title),
-                    trailing: new Image.network(
-                      list[index].thumbnailUrl,
-                      fit: BoxFit.cover,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                  );
-                })
-                );
+        body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text(list[index].title));
+          },
+        )
+        );
   }
 }
